9a34f663057d2fda16c4c31ef4d7b87802e42815
Lakubas <k.headg@gmail.com>
2020-11-18 17:38:24 +0100

    Tenth challenge COMPLETE!

---
+++ small_challenges/tenth_challenge.js
@@ -0,0 +1,11 @@
+const whosPaying = (names) => {
+    let selectRandom = Math.floor((Math.random() * 100)) % names.length;
+
+    let outputText = names[selectRandom] + " is going to buy lunch today!";
+
+    return outputText;
+}
+
+let nameList = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+
+console.log(whosPaying(nameList));
