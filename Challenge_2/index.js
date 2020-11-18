0e58a8b0f629de1e73d4dcc5a698b1e42e37dc95
Lakubas <k.headg@gmail.com>
2020-11-18 19:40:56 +0100

    Challenge_2: Editing CSS styles and internal HTML values using JavaScript.

---
+++ Challenge_2/index.js
@@ -0,0 +1,17 @@
+document.querySelector("ul").lastElementChild.innerHTML = "Third li of ul. :)";
+
+document.getElementsByTagName("li")[2].style.color = "green";
+document.getElementsByTagName("li")[1].style.color = "blue";
+
+document.querySelector("#header1").style.color = "red";
+document.querySelector(".header2").style.color = "green";
+
+document.querySelector("li a").style.background = "green";
+
+document.querySelectorAll("li")[1].innerHTML = "orange";
+
+document.querySelector("ul li a").style.color = "red";
+
+document.querySelector("h1").style.fontSize = "10rem";
+
+document.querySelector("button").style.background = "yellow"
