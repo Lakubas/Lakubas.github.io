02082387dfeb222165178e4215403d7487c0edb1
Lakubas <k.headg@gmail.com>
2020-11-18 16:52:50 +0100

    Eighth challenge complete! :)

---
+++ small_challenges/eighth_challenge.js
@@ -0,0 +1,21 @@
+const isGueast = (guestName) => {
+    var guestList = [
+        "Ala",
+        "Ola",
+        "Pola",
+        "Adam",
+        "Arek",
+        "Kasztan"
+    ];
+    guestName = guestName.slice(0, 1).toUpperCase() + guestName.slice(1).toLowerCase();
+
+    return guestList.includes(guestName)
+}
+
+let inputName = prompt("What is your name?");
+
+if (isGueast(inputName)) {
+    console.log("Wellcome " + inputName);
+} else {
+    console.log("Sorry, maybe next time!")
+}
