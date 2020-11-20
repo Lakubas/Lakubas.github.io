864a22dff824709af966c194c8f4fba1d2556783
Lakubas <k.headg@gmail.com>
2020-11-20 16:52:17 +0100

    Logika aplikacji dla SIMON'a

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-console.log(document.querySelector("h1"));
+import Game from "./modules/game.js"
+
+/*
+Metoda odpowiedzialna na rozpoczęcie rozgrywki.
+ */
+function startGame() {
+    console.log("You start the game SIMON! :)\n Good Luck!");
+    let game = new Game(true);
+    game.start();
+    game.play();
+    initSimonButtons(game);
+
+}
+
+function checkSimonMove() {
+
+    let move = document
+}
+
+/*
+Deklaracja eventu listenera do obslugi nacisniecia klawiszy klawiatury.
+ */
+function initKeyboardButtons() {
+    window.addEventListener("keypress", startGame);
+}
+
+/* Deklaracja eventu listenera do obslugi przyciskow SIMON'a, pozwalająca na
+ * tworzenie sekwencji gracza.
+ */
+function initSimonButtons(game) {
+    let button = document.querySelectorAll(".btn");
+    for (var i = 0; i < button.length; i++) {
+        button[i].addEventListener("click", function() {
+            game.addClickedButtonToSequence(this)
+        });
+    }
+}
+
+initKeyboardButtons();
---
+++ js/modules/displayGame.js
@@ -0,0 +1,35 @@
+/*
+Funkcja odpowiedzialna za odtwarzanie dzwięków.
+ */
+function playSounds(buttonIdName) {
+    let audio = new Audio("../sounds/" + buttonIdName + ".mp3");
+    audio.play();
+}
+
+/*
+Asynchroniczna matoda odpowiedzialna za wyswietlanie sekwencji SIMON
+ */
+async function display(game_sequence) {
+    for (var i = 0; i < game_sequence.length; i++) {
+        let id = game_sequence[i].toString();
+        try {
+            document.querySelector("#" + id).classList.add("btn-active")
+            playSounds(id);
+            await _sleep(500);
+            document.querySelector("#" + id).classList.remove("btn-active")
+        } catch  {}
+    }
+}
+
+/*
+Co to za kod bez metody typu "sleep"> :D
+ */
+function _sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+export {
+    playSounds,
+    display,
+    _sleep
+};
