c5b9f508700ed6e8ebbb8cba62353d8f871c9caa
Lakubas <k.headg@gmail.com>
2020-11-28 22:26:54 +0100

    Small modification for detail section. :)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, {Fragment} from "react";
+
+function Detail(props) {
+    let detail = props.detailInfo;
+    console.log(detail);
+
+    return (
+        <Fragment>
+            <p className="info">{detail.phone}</p>
+            <p className="info">{detail.email}</p>
+        </Fragment>
+    );
+}
 
 function Card(props) {
-	let contact = props.contact;
+    let contact = props.contact;
+    let detailInfo = {
+        phone: contact.phone,
+        email: contact.email
+    };
 
-	return (
-		<div className="card">
-			<div className="top">
-				<h2 className="name">{contact.name}</h2>
-				<img
-					className="circle-img"
-					src={contact.imgURL}
-					alt="avatar_img"
-				/>
-			</div>
-			<div className="bottom">
-				<p className="info">{contact.phone}</p>
-				<p className="info">{contact.email}</p>
-			</div>
-		</div>
-	);
+    return (
+        <div className="card">
+            <div className="top">
+                <h2 className="name">{contact.name}</h2>
+                <img className="circle-img" src={contact.imgURL} alt="avatar_img"/>
+            </div>
+            <div className="bottom">
+                <Detail detailInfo={detailInfo}/>
+            </div>
+        </div>
+    );
 }
 
 export default Card;
