a476b06b12d80bf14bbf043e0aa1530aafe15e76
Lakubas <k.headg@gmail.com>
2020-11-29 16:32:53 +0000

    Complete challenge!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import Header from "./Header/Header";
 import Note from "./Note/Note";
 import Footer from "./Footer/Footer";
-import notes from "./Note/notes"
+
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <Header />
-                {notes.map((noteItem) => 
-                    <Note 
-                    key={noteItem.key} 
-                    note={noteItem} 
-                    />)}
+                <Note />
                 <Footer />
             </div>
         );
--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,12 +1,42 @@
 import React from "react";
+import AddNote from "./AddNote";
+import ItemNote from "./ItemNote";
+// import notes from "./notes"
 
-export default class Note extends React.Component {
-    render() {
-        return (
-            <div className="note">
-                <h1>{this.props.note.title}</h1>
-                <p>{this.props.note.content}</p>
-            </div>
-        );
+export default function Note() {
+
+    const [ note, setNote ] = React.useState([]);
+
+    let onAddNote = (newNote) => {
+        console.log("ADDING");
+        console.log(newNote);
+        setNote((prevNotes) => {
+            return [
+                ...prevNotes,
+                newNote
+            ];
+        });
     }
+
+    let onDeleteNote = (id) => {
+                setNote((prevNotes) => {
+            return prevNotes.filter((note, index) => {
+                return index !== id;
+            });
+        });
+    }
+
+    return (
+        <React.Fragment>
+            <AddNote onSubmit={onAddNote}/>
+            {note.map((noteItem, index) => 
+                <ItemNote 
+                key={index}
+                id={index}
+                note={noteItem} 
+                onDelete={onDeleteNote}
+                />)}
+        </React.Fragment>
+    );
 }
+
