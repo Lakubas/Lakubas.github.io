75dcd019df96ca17d124646d685c3fd34445cbac
Lakubas <k.headg@gmail.com>
2020-11-29 03:08:00 +0100

    Prevent default form behavior!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 
 function App() {
-	const [name, setName] = React.useState("");
-	const [displayName, setDisplayName] = React.useState("");
+    const [name, setName] = React.useState("");
+    const [displayName, setDisplayName] = React.useState("");
 
-	function handleChange(event) {
-		setName(event.target.value);
-	}
+    function handleChange(event) {
+        setName(event.target.value);
+    }
 
-	function handleSubmit(event) {
-		event.preventDefault();
-		setDisplayName(name);
-		setName("");
-	}
+    function handleSubmit(event) {
+        event.preventDefault();
+        setDisplayName(name);
+        setName("");
+    }
 
-	return (
-		<div className="container">
-			<h1>Hello {displayName}</h1>
-			<input
-				onChange={handleChange}
-				type="text"
-				placeholder="What's your name?"
-				value={name}
-			/>
-			<button onClick={handleSubmit}>Submit</button>
-		</div>
-	);
+    return (
+        <div className="container">
+            <h1>Hello {displayName}</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="What's your name?"
+                    value={name}/>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
